613a0e1bd8b4e2baa526976dc89f288493890ad7
juyoung <0620ffff@naver.com>
2025-10-27 13:00:39 +0900

    20251027-3

--- homeworks/DS 11/1.c
+++ homeworks/DS 11/1.c
@@ -1,8 +1,3 @@
-/*
-2025002720 박주영
-본인은 이 소스 파일을 다른 사람의 소스를 복사하지 않고 직접 작성하였습니다.
-*/
-
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -24,34 +19,42 @@ void find(listPointer, listPointer*, int);
 void insert(listPointer*, listPointer, int);
 void delete(listPointer*, listPointer, listPointer);
 void printList(listPointer);
-void delete(listPointer*, listPointer, listPointer);
 
 int main(void) {
     int data;
     listPointer x;
-    listPointer trail;
 
     FILE *fp;
-    if((fp = fopen("input.txt", "r")) == NULL)
-    {
+    if((fp = fopen("input.txt", "r")) == NULL) {
         fprintf(stderr, "cannot open the file");
         exit(EXIT_FAILURE);
     }
+
     puts("The ordered list contains:");
-    while(fscanf(fp, "%d", &data)!=EOF)
-    {
+    while(fscanf(fp, "%d", &data)!=EOF) {
         find(first, &x, data);
         insert(&first, x, data);
     }
     printList(first);
+
     puts("After deleting nodes with data less than and equal to 50");
-    
+
     listPointer temp = first;
-    while(temp->link) {
-        x = temp;
-        temp = temp->link;
-        if(temp->data <= 50) delete(&first, NULL, x);
+    listPointer trail = NULL;
+
+    while (temp) {
+        if (temp->data <= 50) {
+            delete(&first, trail, temp);
+            if (trail == NULL)
+                temp = first;
+            else
+                temp = trail->link;
+        } else {
+            trail = temp;
+            temp = temp->link;
+        }
     }
+
     printList(first);
 }
 
@@ -59,25 +62,18 @@ void find(listPointer first, listPointer *x, int data) {
     listPointer pre = NULL;
     listPointer trav = first;
 
-    if(first == NULL)
-    {
-        //empty list
+    if(first == NULL) {
         *x = NULL;
-    }
-    else
-    {
+    } else {
         if(trav->data > data) {
             *x = NULL;
             return;
         }
-        //non-empty list
         while(trav) {
             pre = trav;
             trav = trav->link;
-
-            if(trav&& trav->data > data ) break;
+            if(trav && trav->data > data) break;
         }
-
         *x = pre;
     }
 }
@@ -87,18 +83,14 @@ void insert(listPointer* first, listPointer x, int data) {
     MALLOC(temp, sizeof(*temp));
     temp->data = data;
 
-    if(*first == NULL) 
-    {
+    if(*first == NULL) {
         temp->link = NULL;
         *first = temp;
-    }
-    else
-    {
+    } else {
         if(x == NULL) {
-           temp->link = *first;
-           *first = temp;
-        }
-        else {
+            temp->link = *first;
+            *first = temp;
+        } else {
             temp->link = x->link;
             x->link = temp;
         }
@@ -109,19 +101,15 @@ void delete(listPointer* first, listPointer trail, listPointer x) {
     if(trail)
         trail->link = x->link;
     else
-        *first  = (*first)->link;
+        *first = (*first)->link;
     free(x);
 }
 
-void printList(listPointer first)
-{
+void printList(listPointer first) {
     printf("The list contains: \n");
-    
     for(int i = 1; first; first = first->link, i++) {
         printf("%4d", first->data);
-        if(!(i%10))putchar('\n');
-     
+        if(!(i%10)) putchar('\n');
     }
     printf("\n");
-        
 }
